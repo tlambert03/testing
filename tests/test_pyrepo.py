e492e59ad19cdb7e0511dc5306e7a55e15c60030
Talley Lambert <talley.lambert@gmail.com>
2022-11-11 16:08:39 -0500

    add qt test

--- tests/test_pyrepo.py
+++ tests/test_pyrepo.py
@@ -3,3 +3,11 @@ from pyrepo import do_a_thing
 
 def test_something():
     assert do_a_thing() is True
+
+
+def test_qt(qtbot):
+    from qtpy.QtWidgets import QWidget
+
+    widget = QWidget()
+    qtbot.addWidget(widget)
+    assert widget.isVisible()
